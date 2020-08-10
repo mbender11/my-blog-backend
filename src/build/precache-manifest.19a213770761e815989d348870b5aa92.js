2e2df901c3a34e281027900e374b563b4d0b4864
Mason Bender <mbender@captechconsulting.com>
2020-08-10 11:05:29 -0600

    first commit

---
+++ src/build/precache-manifest.19a213770761e815989d348870b5aa92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9578c3131d6412ce6550b04a650c48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c252a65c6aaf010b74fc",
+    "url": "/static/css/main.dad2e76d.chunk.css"
+  },
+  {
+    "revision": "b3137f8f841162dc5b2f",
+    "url": "/static/js/2.45200934.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45200934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c252a65c6aaf010b74fc",
+    "url": "/static/js/main.eaecfa59.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
